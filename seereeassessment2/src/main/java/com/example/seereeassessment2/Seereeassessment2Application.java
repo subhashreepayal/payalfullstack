3ebde28c23927afe84b6a053d1fa97b0e2a93c6b
subhashreepayal <subhashreepayal58@gmail.com>
2024-06-30 13:19:25 +0530

    Add files via upload

---
+++ seereeassessment2/src/main/java/com/example/seereeassessment2/Seereeassessment2Application.java
@@ -0,0 +1,13 @@
+package com.example.seereeassessment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seereeassessment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seereeassessment2Application.class, args);
+	}
+
+}
