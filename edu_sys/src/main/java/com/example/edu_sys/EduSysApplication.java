cd013a72e4b897e9b442aa3e231d56384bbc485c
subhashreepayal <subhashreepayal58@gmail.com>
2024-06-30 13:11:57 +0530

    Add files via upload

---
+++ edu_sys/src/main/java/com/example/edu_sys/EduSysApplication.java
@@ -0,0 +1,13 @@
+package com.example.edu_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduSysApplication.class, args);
+	}
+
+}
---
+++ edu_sys/src/test/java/com/example/edu_sys/EduSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edu_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
